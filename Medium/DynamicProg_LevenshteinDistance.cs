a53c12542a22d09c4d46260aaeea361d0d5ad134
Laurent Beauregard <lorems.beauregard@gmail.com>
2019-10-14 18:44:26 -0400

    Dynamic Programming : LevenshteinDistance

---
+++ Medium/DynamicProg_LevenshteinDistance.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Operation
+// 1) Insert character
+// 2) Delete character
+// 3) Substitute character
+
+namespace Questions.Medium
+{
+    [TestFixture]
+    class DynamicProg_LevenshteinDistance
+    {
+        [TestCase("abc", "yabd", ExpectedResult = 2)] // insert "y", substitute "c" for "d"
+        [TestCase("biting", "mitten", ExpectedResult = 4)]
+        // T: O(nm)
+        // S: O(nm)
+        public static int LevenshteinDistance(string str1, string str2)
+        {
+            var matrix = new int[str1.Length + 1, str2.Length + 1];
+
+            for (int x = 0; x < matrix.GetLength(0); x++)
+                matrix[x, 0] = x;
+            for (int y = 0; y < matrix.GetLength(1); y++)
+                matrix[0, y] = y;
+
+            for (int y = 1; y < matrix.GetLength(1); y++)
+            {
+                for (int x = 1; x < matrix.GetLength(0); x++)
+                {
+                    matrix[x, y] = (str1[x - 1] == str2[y - 1])
+                        ? matrix[x - 1, y - 1]
+                        : 1 + Math.Min(matrix[x - 1, y],
+                                Math.Min(
+                                    matrix[x - 1, y - 1],
+                                    matrix[x, y - 1]));
+                }
+            }
+
+            return matrix[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+        }
+        static void Print(int[,] matrix)
+        {
+            for (int y = 0; y < matrix.GetLength(1); y++)
+            {
+                for (int x = 0; x < matrix.GetLength(0); x++)
+                {
+                    Console.Write(matrix[x, y] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Medium/DynamicProg_MinNumberOfCoinsForChange.cs
+++ Medium/DynamicProg_MinNumberOfCoinsForChange.cs
@@ -13,34 +13,57 @@ namespace Questions.Medium
         [TestCase(3, new int[] { 2, 1 }, ExpectedResult = 2)]
         [TestCase(135, new int[] { 39, 45, 130, 40, 4, 1, 60, 75 }, ExpectedResult = 2)]
         [TestCase(135, new int[] { 1, 60, 75 }, ExpectedResult = 2)]
-
         public static int MinNumberOfCoinsForChange(int n, int[] denoms)
         {
-            if (n == 0)
-                return 0;
-
             var minCoins = new int[n + 1];
             Array.Fill(minCoins, int.MaxValue);
             minCoins[0] = 0;
 
             foreach (var denom in denoms)
             {
-                if (denom > n)
-                    continue;
-                minCoins[denom] = 1;
-
                 for (int amount = 1; amount < minCoins.Length; amount++)
                 {
-                    if (denom <= amount)
+                    if (amount < denom)
+                        continue;
+
+                    int rest = amount - denom;
+
+                    if (minCoins[rest] == int.MaxValue)
+                        continue;
+
+                    minCoins[amount] = Math.Min(minCoins[amount], minCoins[rest] + 1);
+                }
+            }
+
+            return minCoins[n] == int.MaxValue ? -1 : minCoins[n];
+        }
+        [TestCase(6, new int[] { 1, 2, 4 }, ExpectedResult = 2)] // 4x2 + 2x1
+        [TestCase(7, new int[] { 1, 5, 10 }, ExpectedResult = 3)] // 1+1 + 1x1 + 1x5
+        [TestCase(0, new int[] { 7, 8 }, ExpectedResult = 0)]
+        [TestCase(7, new int[] { }, ExpectedResult = -1)]
+        [TestCase(3, new int[] { 2, 1 }, ExpectedResult = 2)]
+        [TestCase(135, new int[] { 39, 45, 130, 40, 4, 1, 60, 75 }, ExpectedResult = 2)]
+        [TestCase(135, new int[] { 1, 60, 75 }, ExpectedResult = 2)]
+        public static int MinNumberOfCoinsForChangeV2(int n, int[] denoms)
+        {
+            var minCoins = new int[n + 1];
+            Array.Fill(minCoins, int.MaxValue);
+            minCoins[0] = 0;
+
+            foreach (var denom in denoms)
+            {
+                for (int amount = 1; amount < minCoins.Length; amount++)
+                {
+                    if (amount >= denom)
                     {
-                        var reminderIndex = amount - denom;
-                        if (minCoins[reminderIndex] == int.MaxValue) // reminder match doesn't exist
-                            continue;
-                        int maybeMin = minCoins[reminderIndex] + minCoins[denom];
-                        minCoins[amount] = Math.Min(minCoins[amount], maybeMin);
+                        int rest = amount - denom;
+
+                        if (minCoins[rest] != int.MaxValue)
+                            minCoins[amount] = Math.Min(minCoins[amount], minCoins[rest] + 1);
                     }
                 }
             }
+
             return minCoins[n] == int.MaxValue ? -1 : minCoins[n];
         }
     }
