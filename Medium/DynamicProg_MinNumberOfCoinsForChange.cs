4d2201f0275232185ec04e9a641b80de784dd9c0
Laurent Beauregard <lorems.beauregard@gmail.com>
2019-10-13 23:20:20 -0400

    Dynamic Programming Coins

---
+++ Medium/DynamicProg_MinNumberOfCoinsForChange.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace Questions.Medium
+{
+    class DynamicProg_MinNumberOfCoinsForChange
+    {
+        [TestCase(6, new int[] { 1, 2, 4 }, ExpectedResult = 2)] // 4x2 + 2x1
+        [TestCase(7, new int[] { 1, 5, 10 }, ExpectedResult = 3)] // 1+1 + 1x1 + 1x5
+        [TestCase(0, new int[] { 7, 8 }, ExpectedResult = 0)]
+        [TestCase(7, new int[] { }, ExpectedResult = -1)]
+        [TestCase(3, new int[] { 2, 1 }, ExpectedResult = 2)]
+        [TestCase(135, new int[] { 39, 45, 130, 40, 4, 1, 60, 75 }, ExpectedResult = 2)]
+        [TestCase(135, new int[] { 1, 60, 75 }, ExpectedResult = 2)]
+
+        public static int MinNumberOfCoinsForChange(int n, int[] denoms)
+        {
+            if (n == 0)
+                return 0;
+
+            var minCoins = new int[n + 1];
+            Array.Fill(minCoins, int.MaxValue);
+            minCoins[0] = 0;
+
+            foreach (var denom in denoms)
+            {
+                if (denom > n)
+                    continue;
+                minCoins[denom] = 1;
+
+                for (int amount = 1; amount < minCoins.Length; amount++)
+                {
+                    if (denom <= amount)
+                    {
+                        var reminderIndex = amount - denom;
+                        if (minCoins[reminderIndex] == int.MaxValue) // reminder match doesn't exist
+                            continue;
+                        int maybeMin = minCoins[reminderIndex] + minCoins[denom];
+                        minCoins[amount] = Math.Min(minCoins[amount], maybeMin);
+                    }
+                }
+            }
+            return minCoins[n] == int.MaxValue ? -1 : minCoins[n];
+        }
+    }
+}
---
+++ Medium/DynamicProg_NumberOfWaysToMakeChange.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Questions.Medium
+{
+    class DynamicProg_NumberOfWaysToMakeChange
+    {
+        // ( 6$, [1$, 5$] ) => 2 ( 1x1 + 1x5 and 6x1 )
+        [TestCase(6, new int[] { 1, 5 }, ExpectedResult = 2)]
+        // ( 0$, [1$, 5$, 10$, 25$] ) => 4 ( 1x10, 2x5, (1x5 + 5x1), 10x1 )
+        [TestCase(10, new int[] { 1, 5, 10, 25}, ExpectedResult = 4)]
+        [TestCase(5, new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 7)]
+        // T: O(n * denom)
+        // S: O(n)
+        public static int NumberOfWaysToMakeChange(int n, int[] denoms)
+        {
+            var ways = new int[n+1];
+            ways[0] = 1;
+
+            foreach (var denom in denoms)
+            {
+                for (int amount = 1; amount < ways.Length; amount++)
+                {
+                    if (denom <= amount)
+                    {
+                        ways[amount] += ways[amount - denom];
+                    }
+                }
+                Print(ways);
+            }
+
+            return ways[n];
+        }
+
+        public static void Print(int[] ways)
+        {
+            for (int i = 0; i < ways.Length; i++)
+                Console.Write(i.ToString().PadLeft(2, ' ') + ", ");
+
+            Console.WriteLine();
+
+            for (int i = 0; i < ways.Length; i++)
+                Console.Write(ways[i].ToString().PadLeft(2, ' ') + ", ");
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
+
+// (10$, [1$, 5$, 10$, 25$]) => 4 (1x10, 2x5, (1x5 + 5x1), 10x1)
+
+// 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10    $0
+// 1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+
+// 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10    $1      if (1 <= amount)    ways[amount] += ways[amount - denom];
+// 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1                                ways[amount] += ways[amount - 1];
+
+// 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10    $5      if (5 <= amount)    ways[amount] += ways[amount - denom];
+// 1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  3                                ways[amount] += ways[amount - 5];
+
+// 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10    $10     if (10 <= amount)   ways[amount] += ways[amount - denom];
+// 1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  4                                ways[amount] += ways[amount - 10];
+
+// 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10    $25     if (25 <= amount)   ways[amount] += ways[amount - denom];
+// 1,  1,  1,  1,  1,  2,  2,  2,  2,  2,(4)                                ways[amount] += ways[amount - 25];
+
+
+
+//----------------------------
+//   (5, [1,2,3,4,5]) => 7
+//----------------------------
+// 0,  1,  2,  3,  4,  5,   $1      | (1x5) |            |            |            |            |            |       |
+// 1,  1,  1,  1,  1,  1,           |       |            |            |            |            |            |       |
+//                                  |       |            |            |            |            |            |       |
+// 0,  1,  2,  3,  4,  5,   $2      | (1x5) | (1x1, 2x1) | (1x3, 2x1) |            |            |            |       |
+// 1,  1,  2,  2,  3,  3,           |       |            |            |            |            |            |       |
+//                                  |       |            |            |            |            |            |       |
+// 0,  1,  2,  3,  4,  5,   $3      | (1x5) | (1x1, 2x1) | (1x3, 2x1) | (1x2, 3x1) | (2x1, 3x1) |            |       |
+// 1,  1,  2,  3,  4,  5,           |       |            |            |            |            |            |       |
+//                                  |       |            |            |            |            |            |       |
+// 0,  1,  2,  3,  4,  5,   $4      | (1x5) | (1x1, 2x1) | (1x3, 2x1) | (1x2, 3x1) | (2x1, 3x1) | (1x1, 4x1) |       |
+// 1,  1,  2,  3,  5,  6,           |       |            |            |            |            |            |       |
+//                                  |       |            |            |            |            |            |       |
+// 0,  1,  2,  3,  4,  5,   $5      | (1x5) | (1x1, 2x1) | (1x3, 2x1) | (1x2, 3x1) | (2x1, 3x1) | (1x1, 4x1) | (5x1) |
+// 1,  1,  2,  3,  5,  7,           |       |            |            |            |            |            |       |
