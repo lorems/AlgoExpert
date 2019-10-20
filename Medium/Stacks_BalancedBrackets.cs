1624490d1a82b6b7f129a883f9cc92d32c87aa8b
Laurent Beauregard <lorems.beauregard@gmail.com>
2019-10-20 13:43:44 -0400

    Stacks: BalancedBrackets

---
+++ Medium/Stacks_BalancedBrackets.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questions.Medium
+{
+    [TestFixture]
+    class Stacks_BalancedBrackets
+    {
+        [TestCase("([])(){}(())()()", ExpectedResult = true)]
+        [TestCase("", ExpectedResult = true)]
+        [TestCase("))", ExpectedResult = false)]
+        public static bool BalancedBrackets(string str)
+        {
+            var brackets = new Dictionary<char, char>();
+            brackets.Add(')', '(');
+            brackets.Add(']', '[');
+            brackets.Add('}', '{');
+
+            var stack = new Stack<char>();
+
+            foreach (var c in str)
+            {
+                if (brackets.ContainsKey(c))
+                {
+                    if (stack.Count > 0 && stack.Peek() == brackets[c])
+                        stack.Pop();
+                    else
+                        return false;
+                }
+                else
+                {
+                    stack.Push(c);
+                }                
+            }
+            return stack.Count == 0;
+        }
+    }
+}
