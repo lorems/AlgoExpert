80e3af84ef7f111784b2bb4a12d6e1417aa12e6a
Laurent Beauregard <lorems.beauregard@gmail.com>
2019-11-02 16:01:49 -0400

    Arrays: MinRewards - remove version with duplicate value in inputs

--- xHard/Arrays_MinRewards.cs
+++ xHard/Arrays_MinRewards.cs
@@ -63,46 +63,6 @@ namespace Questions.xHard
         [TestCase(new int[] { 1, 2 }, ExpectedResult = 3)]
         [TestCase(new int[] { 2, 1 }, ExpectedResult = 3)]
         [TestCase(new int[] { 3, 2, 1 }, ExpectedResult = 6)]
-        [TestCase(new int[] { 800, 400, 20, 10, 30, 60, 70, 90, 17,
-                              21, 22, 13, 12, 11, 8, 4, 2, 1, 3, 6,
-                              7, 9, 0, 68, 55, 67, 57, 60, 51, 661,
-                              50, 65, 53 }, ExpectedResult = 93)] // include duplicate score
-        // T: O(n(log(n))
-        public static int MinRewardsV1_NonUnique(int[] scores)
-        {
-            var indexDic = new Dictionary<int, List<int>>();
-            var gifs = new int[scores.Length];
-            var sortedScores = scores.Clone() as int[];
-            Array.Sort(sortedScores);
-
-            for (int i = 0; i < scores.Length; i++)
-            {
-                if (!indexDic.ContainsKey(scores[i]))
-                    indexDic[scores[i]] = new List<int>();
-
-                indexDic[scores[i]].Add(i);
-            }
-
-            foreach (var sc in sortedScores)
-            {
-                var positions = indexDic[sc];
-                foreach (var position in positions)
-                {
-                    int leftGift = position > 0 ? gifs[position - 1] : 0;
-                    int myGift = gifs[position];
-                    int rightGift = position < scores.Length - 1 ? gifs[position + 1] : 0;
-
-                    gifs[position] = Math.Max(leftGift, rightGift) + 1;
-                }
-            }
-
-            return gifs.Sum();
-        }
-
-        [TestCase(new int[] { 8, 4, 2, 1, 3, 6, 7, 9, 5 }, ExpectedResult = 25)] // [4, 3, 2, 1, 2, 3, 4, 5, 1]
-        [TestCase(new int[] { 1, 2 }, ExpectedResult = 3)]
-        [TestCase(new int[] { 2, 1 }, ExpectedResult = 3)]
-        [TestCase(new int[] { 3, 2, 1 }, ExpectedResult = 6)]
         [TestCase(new int[] { 3, 2, 1, 44, 55, 33, 22 }, ExpectedResult = 14)]
         // T: O(n) S: O(n)
         // T: O(n^2) S: O(n)
